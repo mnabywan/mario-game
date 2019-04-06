761d79e939c3f3a09f93d436880e1362d5311ffe
MateuszNabywaniec <nabywaniec@student.agh.edu.pl>
2019-04-06 18:26:21 +0200

    first commit

---
+++ mario.py
@@ -0,0 +1,65 @@
+import pygame
+
+
+class Mario(object):
+
+    def __init__(self, game):
+        self.game = game
+
+        size = self.game.screen.get_size()
+        #self.pos = Vector2(0, 600)
+        self.width = 30
+        self.height = 50
+        self.x = 0
+        self.y = game.screen_height - self.height - 95
+        self.speed = 1.0
+        self.vel = 5
+        self.isJump = False
+        self.jumpCount = 10
+     #   self.acc = Vector2(0, 0)
+     #   self.vel = Vector2(0,0)
+
+    def add_force(self, force):
+        self.acc += force
+
+    def draw(self):
+        rect =  pygame.Rect(self.x,self.y, self.width, self.height)
+        pygame.draw.rect(self.game.screen, (255,0,0),rect)
+
+
+    def move(self):
+        pressed = pygame.key.get_pressed()
+
+
+        if pressed[pygame.K_RIGHT] and self.x < self.game.screen_width - self.width - self.vel:
+            self.x += self.vel
+        #    self.add_force(Vector2(self.speed,0))
+
+        if pressed[pygame.K_LEFT] and self.x > self.vel:
+            self.x -= self.vel
+        #    self.add_force(Vector2(-self.speed,0))
+
+        if not(self.isJump):
+
+            if pressed[pygame.K_x]:
+                self.isJump = True
+
+        else:
+            if self.jumpCount >= -10:
+                neg = 1
+                if self.jumpCount < 0:
+                    neg = -1
+                self.y -= (self.jumpCount ** 2) * 0.5 * neg
+                self.jumpCount -=1
+
+            else:
+                self.isJump = False
+                self.jumpCount = 10
+
+            #physicks
+        #self.vel*=0.6
+
+        #self.vel += self.acc
+        #self.pos += self.vel
+        #self.acc *= 0
+
---
+++ run.py
@@ -0,0 +1,57 @@
+import pygame, sys
+from mario import Mario
+
+
+class Game(object):
+
+    def redraw(self):
+        self.screen.blit(self.bg, (0,0))
+        #self.screen.fill((0, 0, 0))
+        self.draw()
+        pygame.display.flip()
+
+
+    def __init__(self):
+        # cONFIG
+        self.tps_max = 70.0
+
+        #init
+        pygame.init()
+        self.screen_height = 601
+        self.screen_width = 1641
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.bg = pygame.image.load('/home/mateusz/PycharmProjects/Mario/graphics/bg.jpg')
+        self.tps_clock = pygame.time.Clock()
+        self.tps_delta = 0.0
+
+        self.mario = Mario(self)
+
+
+        while True:
+            # Handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+                elif event.type == pygame.KEYDOWN and event.key ==  pygame.K_ESCAPE:
+                    sys.exit(0)
+
+            # Ticking
+            self.tps_delta += self.tps_clock.tick() / 1000.0
+            while self.tps_delta > 1 / self.tps_max:
+                self.tick()
+                self.tps_delta -= 1 / self.tps_max
+
+            #Drawing
+            self.redraw()
+
+
+    def tick(self):
+        self.mario.move()
+
+    def draw(self):
+        self.mario.draw()
+        
+
+
+if __name__ == "__main__":
+    Game()

193d72edf839893ba993446a1b5a80ae1947bee5
veron <veron>
2019-04-10 15:08:06 +0200

    camera+background_sprites

---
+++ level1.py
@@ -0,0 +1,77 @@
+import pygame
+from Sprite import *
+
+class Level1:
+    def __init__(self, game, width, heigth):
+        self.game = game
+        self.mario = game.mario
+        self.init_background(self.mario, width, heigth)
+
+    def init_background(self, mario, width, heigth):
+        self.screen_height = heigth
+        self.screen_width = width
+        self.bg_pos = [0, 0]
+        self.level_width = 6784
+        self.level_heigth = self.screen_height
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.bg = pygame.image.load('C:/Users/Veronika/Desktop/Informatyka/Sem 4/Python/MarioTest/graphics/level_1.png')
+        self.bg = pygame.transform.scale(self.bg, (self.level_width, self.level_heigth))
+        self.setup_background_elements()
+
+
+
+    def draw_background(self):
+        self.delta_x = -self.bg_pos[0]
+        if self.mario.vel[0] and self.mario.inthemiddle and self.mario.coordinate_x < self.level_width - self.screen_width * 0.5:
+            self.bg_pos[0] = -self.mario.coordinate_x + self.screen_width * 0.5
+        self.delta_x += self.bg_pos[0]
+        self.update_background_elements()
+        self.screen.blit(self.bg, self.bg_pos)
+        self.bg_elem_group.draw(self.screen)
+        #self.screen.blit(self.bg, self.bg_pos)
+
+
+    def read_map(self):
+        f = open('C:/Users/Veronika/Desktop/Informatyka/Sem 4/Python/MarioTest/graphics/map.txt')
+        self.map = f.readlines()
+        for i in range(0, 14, 1):
+            self.map[i] = self.map[i].split()
+
+    def setup_background_elements(self):
+        self.read_map()
+        self.bg_elem_group = pygame.sprite.Group()
+        for i in range(0, 14, 1):
+            for j in range(0, 212, 1):
+                if self.map[i][j] != "0":
+                    element = Map(32*j, 32*i, 32, 32)
+                    self.bg_elem_group.add(element)
+
+    def check_mario_collisions_x(self):
+        bg_elem = pygame.sprite.spritecollideany(self.mario, self.bg_elem_group)
+        if bg_elem:
+            if self.mario.rect.x < bg_elem.rect.left:
+                self.mario.rect.right = bg_elem.rect.left
+
+            elif self.mario.rect.x < bg_elem.rect.right:
+                self.mario.rect.left = bg_elem.rect.right
+
+            self.mario.coordinate_x -= self.mario.vel[0]
+
+
+    def check_mario_collisions_y(self):
+        bg_elem = pygame.sprite.spritecollideany(self.mario, self.bg_elem_group)
+        if bg_elem:
+
+            if self.mario.rect.y < bg_elem.rect.bottom:
+                self.mario.rect.top = bg_elem.rect.bottom
+
+            elif self.mario.rect.y > bg_elem.rect.top:
+                self.mario.rect.bottom = bg_elem.rect.top
+
+    def update_background_elements(self):
+        for element in self.bg_elem_group:
+            element.rect.x += self.delta_x
+
+        #self.check_mario_collisions_y()
+        #self.check_mario_collisions_x()
+
--- run.py
+++ run.py
@@ -1,41 +1,44 @@
 import pygame, sys
 from mario import Mario
 from brick import Brick
+from level1 import Level1
+from Sprite import Map
 
 
 class Game(object):
 
     def redraw(self):
-        self.screen.blit(self.bg, (0,0))
-        #self.screen.fill((0, 0, 0))
+
+        self.level.screen.fill((0, 0, 0))
         self.draw()
         pygame.display.flip()
-
 
     def __init__(self):
         # cONFIG
         self.tps_max = 70.0
 
-        #init
+        # init
         pygame.init()
-        self.screen_height = 601
-        self.screen_width = 1641
-        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-        self.bg = pygame.image.load('C:/Users/Veronika/Desktop/Informatyka/mario.png')
-        #self.bg = pygame.image.load('/home/mateusz/PycharmProjects/Mario/graphics/bg.jpg')
+
+        # self.screen_height = 601
+        # self.screen_width = 1641
+        self.screen_height = 464
+        self.screen_width = 640
+        self.mario = Mario(self)
+        self.level = Level1(self, self.screen_width, self.screen_height)
+
+        # self.bg = pygame.image.load('/home/mateusz/PycharmProjects/Mario/graphics/bg.jpg')
         self.tps_clock = pygame.time.Clock()
         self.tps_delta = 0.0
 
-        self.mario = Mario(self)
         self.brick1 = Brick(self, 120, 140)
-
 
         while True:
             # Handle events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit(0)
-                elif event.type == pygame.KEYDOWN and event.key ==  pygame.K_ESCAPE:
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     sys.exit(0)
 
             # Ticking
@@ -44,18 +47,17 @@ class Game(object):
                 self.tick()
                 self.tps_delta -= 1 / self.tps_max
 
-            #Drawing
+            # Drawing
             self.redraw()
-
 
     def tick(self):
         self.mario.move()
 
     def draw(self):
+        self.level.draw_background()
         self.mario.draw()
         if self.brick1.is_visible:
             self.brick1.draw()
-        
 
 
 if __name__ == "__main__":
